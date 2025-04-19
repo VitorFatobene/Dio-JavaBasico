fda89dfd211841a7e392eee0e66f9231ad1f9980
Vitor Fatobene <vitorfatobene@gmail.com>
2025-04-19 00:22:00 -0300

    tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final double PI = 3.14;
+        System.out.print(PI);
+
     }
 }
